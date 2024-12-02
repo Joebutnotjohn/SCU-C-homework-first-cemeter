01e517e68331d1fc76358814394f627cc2ad91c7
Joebutnotjohn <ThewayISing4U@163.com>
2024-12-02 08:16:58 +0800

    Create 统计最多的单词.c
    描述
    统计一段文本中出现次数最多的英文单词。单词不区分大小写，即Hello和hello表示同一个单词。单词和单词之间由一个或多个非字母字符隔开。
    
    输入
    输入数据为一段文本，可能包含若干行，直到文件末尾(EOF)为止。即只有一组测试用例。
    
    输出
    输出该段文本中出现最多的单词(全变换成小写字母)，及该单词出现的次数，中间用空格隔开。如果出现次数最多的单词有多个，输出字典序最大(全转换成小写字母比较)的那个。
    
    样例1
    输入
    
    Is this a   very hard problem?
    No, it is not!
    输出
    
    is 2
    样例2
    输入
    
    I like   computer.   I like Computer Science.
    I like School   of Computer Science.
    I like   Sichuan University.
    输出
    
    like 4
    提示
    如果用getchar()或者scanf()函数读入数据，那么当返回EOF时表示已经读取到文件末尾。
    如果用fgets()函数读入数据，那么返回NULL时表示已经读取到文件末尾。

---
+++ 统计最多的单词.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+//定义一个结构体，这样就可以将一个单词与一个值相对应，不会出现定义很多个变量来存每个单词分别的数量了
+
+typedef struct{
+    char word[100];
+    int count;
+}Wordfrequency;
+
+int search(Wordfrequency Wordlist[],char* word,int wordcount){
+    for(int i=0;i<wordcount;i++){
+        if(strcmp(Wordlist[i].word,word)==0){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    char text[10000];
+    char ch;
+    int i=0;
+    while((ch=getchar())!=EOF){
+        text[i++]=ch;
+    }
+    text[i]='\0';
+    //将大写全部转换为小写
+    int len=strlen(text);
+    for(int i=0;i<len;i++){
+        if(isupper(text[i])){
+            text[i]=text[i]+' ';
+        }
+    }
+    //初始化结构体  
+    Wordfrequency WORD[5000];
+    char word[100];
+    int wordcount=0;//表示结构体中已找到的单词个数，即结构体中的索引
+    //先获取一个单独的单词
+    int wordindex=0;
+    for(int j=0;j<len;j++){
+        if(isalpha(text[j])){
+            word[wordindex++]=text[j];
+        }else {
+            if(wordindex>0){
+        word[wordindex]='\0';wordindex=0;//对其清零，清零后再存储单词
+        //再从已统计的单词中查找是否已经找到了
+        int num=search(WORD,word,wordcount);
+        if(num==-1){
+            strcpy(WORD[wordcount].word,word);//将word放置到结构体的word部分里。
+            WORD[wordcount].count=1;//增加单词出现频率个数
+            wordcount++;//已统计的单词个数增加，即结构体组索引增加
+        }
+        if(num!=-1){
+            WORD[num].count++;//找到对应索引并增加频率
+        }
+        
+            }
+        }
+    }
+
+    //全部找完之后，再比较count最大的
+    for(int m=0;m<wordcount-1;m++){
+        if(WORD[m].count>WORD[m+1].count){
+            //交换结构体中的位置
+            Wordfrequency temp=WORD[m];
+            WORD[m]=WORD[m+1];
+            WORD[m+1]=temp;
+        }
+        if(WORD[m].count==WORD[m+1].count){
+            continue;
+        }
+    }
+    // 按照频率从大到小排序，如果频率相同，则按照字典序排序
+    for (int i = 0; i < wordcount - 1; i++) {
+        for (int j = i + 1; j < wordcount; j++) {
+            if (WORD[i].count < WORD[j].count || 
+                (WORD[i].count == WORD[j].count && strcmp(WORD[i].word, WORD[j].word) < 0)) {
+                // 如果频率较小或频率相同但字典序较大，交换
+                Wordfrequency temp = WORD[i];
+                WORD[i] = WORD[j];
+                WORD[j] = temp;
+            }
+        }
+    }
+
+    // 输出字典序最大的单词及其频率
+    printf("%s %d\n", WORD[0].word, WORD[0].count);
+
+    return 0;
+    
+}
+//我的错误排序。。。，。
+/*int number=0;
+    for(int x=0;x<wordcount;x++){
+        if(WORD[x].count==WORD[wordcount-1].count){
+            number++;
+        }
+    }
+    if(number>1){
+        for(int h=wordcount-1-number+1;h<wordcount-1;h++){
+            int NUM=strcmp(WORD[h].word,WORD[h+1].word);
+            if(NUM>0){
+                Wordfrequency temp_string=WORD[h];
+                WORD[h]=WORD[h+1];
+                WORD[h+1]=temp_string;
+            }
+        }
+        //输出字典序
+    printf("%s %d",WORD[wordcount-1].word,WORD[wordcount-1].count);
+    }
+    if(number==1){
+        printf("%s %d",WORD[wordcount-1].word,WORD[wordcount-1].count);
+    }
+    */
